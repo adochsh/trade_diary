0beba57945f805aa921840169c93e6bcd22c68f3
aminadoo <91411528+adochsh@users.noreply.github.com>
2022-06-06 16:49:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from multiapp import MultiApp
 # import your app modules here
-from apps import main, analysis#, smain
+from apps import smain#, analysis#, smain
 app = MultiApp()
 st.set_page_config(layout="wide")
 
 # Add all your application here
-app.add_app("Main Page", main.app)
-app.add_app("Analysis", analysis.app)
-# app.add_app("bugreport", smain.app)
+# app.add_app("Main Page", main.app)
+# app.add_app("Analysis", analysis.app)
+app.add_app("bugreport", smain.app)
 
 # The main app
 app.run()
