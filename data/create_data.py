00cd22d8db985629fe245505b1fa836013a5c303
aminadoo <91411528+adochsh@users.noreply.github.com>
2022-06-06 16:53:47 +0300

    Update create_data.py

--- data/create_data.py
+++ data/create_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
 import numpy as np
-def create_table():
-    df = pd.read_excel('/Users/amina/Library/DBeaverData/workspace6/General/Scripts/ries/data.xlsx')
-    return df
+# def create_table():
+#     df = pd.read_excel('/Users/amina/Library/DBeaverData/workspace6/General/Scripts/ries/data.xlsx')
+#     return df
