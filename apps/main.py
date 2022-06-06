6d50679d73fb051b67a2d2e9ad241a8151ef281c
adochsh <adochsh@gmail.com>
2022-06-06 18:28:28 +0300

    read google sheet

--- apps/main.py
+++ apps/main.py
@@ -16,3 +16,19 @@ with st.sidebar.expander("Identifier", expanded=False):
     # articles = {"LM code , ADEO code , EAN"}
     articles_join = []
     articles = st.multiselect("heyo", articles_join)
+
+# Create a connection object.
+conn = connect()
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["public_gsheets_url"]
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
